32339a205ef5d0aa921b35a22dda675cf48fe54a
Bob <dash78231@gmail.com>
2025-06-18 14:45:23 +0800

    nested route reactive (no need to F5)

--- src/app/product-detail.component.ts
+++ src/app/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, computed, inject } from "@angular/core";
+import { Component, effect, inject, signal } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
@@ -9,12 +9,20 @@ import { ActivatedRoute } from "@angular/router";
     template:`
     <div class="border p-4 rounded bg-gray-100">
         <h2 class="text-xl font-semibold">Product Detail</h2>
-        <p class="mt-2">Selected Product ID: <strong>{{id()}}</strong></p>
+        <p class="mt-2">Selected Product ID: <strong>{{productId()}}</strong></p>
     </div>
     `
 })
 
 export class ProductDetailComponent {
     private route = inject(ActivatedRoute);
-    readonly id = computed(()=>this.route.snapshot.paramMap.get('id'));
+    readonly productId = signal<string | null>(null);
+
+    constructor() {
+        effect(()=>{
+            this.route.paramMap.subscribe(params=>{
+                this.productId.set(params.get('id'));
+            });
+        });
+    }
 }

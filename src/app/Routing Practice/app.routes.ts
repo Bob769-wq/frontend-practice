6f1db24c78588dffc2d77ab9d72de6ab218d8694
Bob <dash78231@gmail.com>
2025-06-23 14:27:09 +0800

    clean the workplace set a folder

--- src/app/Routing Practice/app.routes.ts
+++ src/app/Routing Practice/app.routes.ts
@@ -1,15 +1,15 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './home.component';
-import { AboutComponent } from './about.component';
-import { SearchComponent } from './search.component';
-import { ShopComponent } from './shop.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { NotFoundComponent } from './not-found.component';
+import { HomeComponent } from './pages/home.component';
+import { AboutComponent } from './pages/about.component';
+import { SearchComponent } from './pages/search.component';
+import { ShopComponent } from './pages/shop.component';
+import { ProductDetailComponent } from './pages/product-detail.component';
+import { NotFoundComponent } from './pages/not-found.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: HomeComponent },
   {path:'profile',
-    loadComponent:()=>import('./profile.component').then(m=>m.ProfileComponent),
+    loadComponent:()=>import('./pages/profile.component').then(m=>m.ProfileComponent),
   },
   { path: 'about', component: AboutComponent },
   { path: 'search', component: SearchComponent },

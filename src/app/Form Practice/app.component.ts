2904a51aa7a2a5c66ed8e61d7bf1e02d736e2c60
Bob <dash78231@gmail.com>
2025-06-23 17:12:54 +0800

    Grouping form controls

--- src/app/Form Practice/app.component.ts
+++ src/app/Form Practice/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { NameEditorComponent } from "./name-editor.component";
+import { ProfileEditorComponent } from "./profile-editor.component";
 
 @Component ({
     selector:'app-root',
-    imports:[NameEditorComponent],
+    imports: [NameEditorComponent, ProfileEditorComponent],
     template:`
     <h1>Reactive Forms</h1>
     <app-name-editor/>
+    <app-profile-editor>
     `
 })
 

c0ed287c1629cd6948eb9bc139e62efdc9cb8ed4
Bob <dash78231@gmail.com>
2025-07-01 09:22:15 +0800

    做YouTube囉

---
+++ src/app/YouTube/app.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-root',
+    standalone:true,
+    imports:[CommonModule],
+    template:`
+    
+    `
+})
+export class AppComponent {
+    
+}

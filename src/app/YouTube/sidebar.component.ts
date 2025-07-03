756d3f6e0891553b1968da2ad83ab1bed932ec0f
Bob <dash78231@gmail.com>
2025-07-03 17:30:32 +0800

    routerlink active  / 擴大點擊範圍

--- src/app/YouTube/sidebar.component.ts
+++ src/app/YouTube/sidebar.component.ts
@@ -15,7 +15,9 @@ import { CommonModule } from "@angular/common";
     ],
     template:`
     <nav class="flex flex-col items-center space-y-6 p-4">
-        <a routerLink="/" class="flex flex-col items-center space-y-1">
+        <a routerLink="/" routerLinkActive="bg-gray-200" [routerLinkActiveOptions]="{exact:true}"
+        class="flex flex-col items-center w-full 
+        rounded-xl hover:bg-gray-100 transition space-y-1">
             <mat-icon>home</mat-icon>
             <span class="text-xs">首頁</span>
         </a>
@@ -23,7 +25,9 @@ import { CommonModule } from "@angular/common";
             <mat-icon>play_arrow</mat-icon>
             <span class="text-xs">Shorts</span>
         </a>
-        <a routerLink="/subscriptions" class="flex flex-col items-center space-y-1">
+        <a routerLink="/subscriptions" routerLinkActive="bg-gray-200"
+        class="flex flex-col items-center w-full
+        rounded-xl hober:bg-gray-100 transition space-y-1">
             <mat-icon>subscriptions</mat-icon>
             <span class="text-xs">訂閱內容</span>
         </a>
